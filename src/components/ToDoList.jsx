f873248670f2ff01d572b3d5880d17c4ca40bdb1
Ryan Kretch <rkretch91@gmail.com>
2018-11-05 16:17:36 +0100

    changes made

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,19 +3,28 @@ import ToDoItem from './ToDoItem'
 
 class ToDoList extends React.Component {
     filterItems = () => {
+        const filtered = [];
 
-        //Result of an array of filter items
+        for (let id in this.props.toDoItems) {
+            let item = this.props.toDoItems[id];
+
+            if (this.props.filter === "all" || (this.props.filter === "done" && item.done === true) || (this.props.filter === "undone" && item.done === false)) {
+                filtered.push(item);
+            }
+        }
+        return filtered
     }
+    
     render() {
         return (
             <div className="todo-list">
                 <table className="todo-items table table-borderless">
                     <tbody>
                         {/* Convert object keys to be able to use filterItems function */}
-                        {Object.keys(this.props.toDoItems).map(item =>
+                        {this.filterItems().map(item =>
                             <ToDoItem
-                                key={`item-${item}`}
-                                item={this.props.toDoItems[item]}
+                                key={`item-${item.uuid}`}
+                                item={item}
                                 updateToDoText={this.props.updateToDoText}
                                 removeItem={this.props.removeItem}
                                 toggleToDoItem={this.props.toggleToDoItem}
